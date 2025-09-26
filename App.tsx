0b65b6bfcef9c71f99d892835a9ede5da372baf2
markwon <markw@naver.com>
2025-09-26 23:58:51 +0900

    change app

--- App.tsx
+++ App.tsx
@@ -16,42 +16,42 @@ import SignUpScreen from "./src/screens/SignUpScreen";
 
 
 // ✅ Info 화면들 (8개)
-import Info1GuestScreen from "./src/screens/Info1GuestScreen";
-import Info2GuestScreen from "./src/screens/Info2GuestScreen";
-import Info3GuestScreen from "./src/screens/Info3GuestScreen";
-import Info4GuestScreen from "./src/screens/Info4GuestScreen";
-import Info5GuestScreen from "./src/screens/Info5GuestScreen";
-import Info6GuestScreen from "./src/screens/Info6GuestScreen";
-import Info7GuestScreen from "./src/screens/Info7GuestScreen";
-import Info8GuestScreen from "./src/screens/Info8GuestScreen";
+//import Info1GuestScreen from "./src/screens/Info1GuestScreen";
+//import Info2GuestScreen from "./src/screens/Info2GuestScreen";
+//import Info3GuestScreen from "./src/screens/Info3GuestScreen";
+//import Info4GuestScreen from "./src/screens/Info4GuestScreen";
+//import Info5GuestScreen from "./src/screens/Info5GuestScreen";
+//import Info6GuestScreen from "./src/screens/Info6GuestScreen";
+//import Info7GuestScreen from "./src/screens/Info7GuestScreen";
+//import Info8GuestScreen from "./src/screens/Info8GuestScreen";
 
 // ✅ Q&A 화면들 (7개)
-import QAMainGuestScreen from "./src/screens/QAMainGuestScreen";
-import QAMemberGuestScreen from "./src/screens/QAMemberGuestScreen";
-import QANumberGenGuestScreen from "./src/screens/QANumberGenGuestScreen";
-import QAAIStatsGuestScreen from "./src/screens/QAAIStatsGuestScreen";
-import QAPremiumGuestScreen from "./src/screens/QAPremiumGuestScreen";
-import QAEtcGuestScreen from "./src/screens/QAEtcGuestScreen";
-import QAInquiryGuestScreen from "./src/screens/QAInquiryGuestScreen";
+//import QAMainGuestScreen from "./src/screens/QAMainGuestScreen";
+//import QAMemberGuestScreen from "./src/screens/QAMemberGuestScreen";
+//import QANumberGenGuestScreen from "./src/screens/QANumberGenGuestScreen";
+//import QAAIStatsGuestScreen from "./src/screens/QAAIStatsGuestScreen";
+//import QAPremiumGuestScreen from "./src/screens/QAPremiumGuestScreen";
+//import QAEtcGuestScreen from "./src/screens/QAEtcGuestScreen";
+//import QAInquiryGuestScreen from "./src/screens/QAInquiryGuestScreen";
 
 // ✅ 패턴 화면들 (6개)
-import Patterns1to10GuestScreen from "./src/screens/Patterns1to10GuestScreen";
-import Patterns11to20GuestScreen from "./src/screens/Patterns11to20GuestScreen";
-import Patterns21to30GuestScreen from "./src/screens/Patterns21to30GuestScreen";
-import Patterns31to40GuestScreen from "./src/screens/Patterns31to40GuestScreen";
-import Patterns41to45GuestScreen from "./src/screens/Patterns41to45GuestScreen";
-import Patterns46to55GuestScreen from "./src/screens/Patterns46to55GuestScreen";
+//import Patterns1to10GuestScreen from "./src/screens/Patterns1to10GuestScreen";
+//import Patterns11to20GuestScreen from "./src/screens/Patterns11to20GuestScreen";
+//import Patterns21to30GuestScreen from "./src/screens/Patterns21to30GuestScreen";
+//import Patterns31to40GuestScreen from "./src/screens/Patterns31to40GuestScreen";
+//import Patterns41to45GuestScreen from "./src/screens/Patterns41to45GuestScreen";
+//import Patterns46to55GuestScreen from "./src/screens/Patterns46to55GuestScreen";
 
 // ✅ 개별 패턴 화면들 (9개)
-import Pattern47GuestScreen from "./src/screens/Pattern47GuestScreen";
-import Pattern48GuestScreen from "./src/screens/Pattern48GuestScreen";
-import Pattern49GuestScreen from "./src/screens/Pattern49GuestScreen";
-import Pattern50GuestScreen from "./src/screens/Pattern50GuestScreen";
-import Pattern51GuestScreen from "./src/screens/Pattern51GuestScreen";
-import Pattern52GuestScreen from "./src/screens/Pattern52GuestScreen";
-import Pattern53GuestScreen from "./src/screens/Pattern53GuestScreen";
-import Pattern54GuestScreen from "./src/screens/Pattern54GuestScreen";
-import Pattern55GuestScreen from "./src/screens/Pattern55GuestScreen";
+//import Pattern47GuestScreen from "./src/screens/Pattern47GuestScreen";
+//import Pattern48GuestScreen from "./src/screens/Pattern48GuestScreen";
+//import Pattern49GuestScreen from "./src/screens/Pattern49GuestScreen";
+//import Pattern50GuestScreen from "./src/screens/Pattern50GuestScreen";
+//import Pattern51GuestScreen from "./src/screens/Pattern51GuestScreen";
+//import Pattern52GuestScreen from "./src/screens/Pattern52GuestScreen";
+//import Pattern53GuestScreen from "./src/screens/Pattern53GuestScreen";
+//import Pattern54GuestScreen from "./src/screens/Pattern54GuestScreen";
+//import Pattern55GuestScreen from "./src/screens/Pattern55GuestScreen";
 
 // ✅ 모달 컴포넌트들
 import AdPlaceholder from "./src/components/AdPlaceholder";
@@ -107,7 +107,7 @@ export default function App() {
 
         <Stack.Screen name="PremiumStack" component={PremiumNavigator} />
 
-        {/* =================== Info 화면들 =================== */}
+        {/* =================== Info 화면들 =================== 
         <Stack.Screen name="Info1Guest" component={Info1GuestScreen} />
         <Stack.Screen name="Info2Guest" component={Info2GuestScreen} />
         <Stack.Screen name="Info3Guest" component={Info3GuestScreen} />
@@ -116,8 +116,8 @@ export default function App() {
         <Stack.Screen name="Info6Guest" component={Info6GuestScreen} />
         <Stack.Screen name="Info7Guest" component={Info7GuestScreen} />
         <Stack.Screen name="Info8Guest" component={Info8GuestScreen} />
-
-        {/* =================== Q&A 화면들 =================== */}
+        */}
+        {/* =================== Q&A 화면들 =================== 
         <Stack.Screen name="QAMainGuest" component={QAMainGuestScreen} />
         <Stack.Screen name="QAMemberGuest" component={QAMemberGuestScreen} />
         <Stack.Screen name="QANumberGenGuest" component={QANumberGenGuestScreen} />
@@ -125,16 +125,16 @@ export default function App() {
         <Stack.Screen name="QAPremiumGuest" component={QAPremiumGuestScreen} />
         <Stack.Screen name="QAEtcGuest" component={QAEtcGuestScreen} />
         <Stack.Screen name="QAInquiryGuest" component={QAInquiryGuestScreen} />
-
-        {/* =================== 패턴 화면들 =================== */}
+        */}
+        {/* =================== 패턴 화면들 =================== 
         <Stack.Screen name="Patterns1to10Guest" component={Patterns1to10GuestScreen} />
         <Stack.Screen name="Patterns11to20Guest" component={Patterns11to20GuestScreen} />
         <Stack.Screen name="Patterns21to30Guest" component={Patterns21to30GuestScreen} />
         <Stack.Screen name="Patterns31to40Guest" component={Patterns31to40GuestScreen} />
         <Stack.Screen name="Patterns41to45Guest" component={Patterns41to45GuestScreen} />
         <Stack.Screen name="Patterns46to55Guest" component={Patterns46to55GuestScreen} />
-
-        {/* =================== 개별 패턴 화면들 =================== */}
+        */}
+        {/* =================== 개별 패턴 화면들 =================== 
         <Stack.Screen name="Pattern47Guest" component={Pattern47GuestScreen} />
         <Stack.Screen name="Pattern48Guest" component={Pattern48GuestScreen} />
         <Stack.Screen name="Pattern49Guest" component={Pattern49GuestScreen} />
@@ -144,7 +144,7 @@ export default function App() {
         <Stack.Screen name="Pattern53Guest" component={Pattern53GuestScreen} />
         <Stack.Screen name="Pattern54Guest" component={Pattern54GuestScreen} />
         <Stack.Screen name="Pattern55Guest" component={Pattern55GuestScreen} />
-      
+        */}
       </Stack.Navigator>
     </NavigationContainer>
   );
